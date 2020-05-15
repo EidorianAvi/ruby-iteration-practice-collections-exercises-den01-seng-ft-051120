d8c10e8de05d4918de8432e55d8545542aad654c
Rene Avila <reneavila1993@gmail.com>
2020-05-15 03:58:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(array)
+  new = array.sort
+end
 
+def sort_array_desc(array)
+  output = array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort 
+end
