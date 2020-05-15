e6365395fcd5e549f0b68776bdcaa5b02e02fafe
Rene Avila <reneavila1993@gmail.com>
2020-05-15 04:05:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,5 +23,11 @@ def reverse_array(array)
 end 
 
 def kesha_maker(array)
-  
+  output = []
+  array.each do |word|
+    word_split = word.split ""
+    word_split[2] = "$"
+    output << word_split.join
+  end
+  output
 end
